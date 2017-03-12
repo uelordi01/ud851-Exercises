35deda3e6558343670f2cfe0d76d2bd2fd6cf96f
uelordi <elordiae@gmail.com>
2017-03-12 22:55:46 +0100

    creating table

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -1,15 +1,35 @@
 package com.example.android.waitlist.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 // TODO (1) extend the SQLiteOpenHelper class
-public class WaitlistDbHelper {
+public class WaitlistDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "waitlist.db";
+    private static final String DATABASE_VERSION = "1";
 
-    // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
+    public WaitlistDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
 
-    // TODO (3) Create a static final int called DATABASE_VERSION and set it to 1
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_WAITLIST_TABLE = "create "+DATABASE_NAME
+                            +"("+ WaitlistContract.WaitlistEntry._ID+","
+                            +"INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                            WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME +" TEXT NOT NULL, "+
+                            WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE + " INTEGER NOT NULL "+
+                            WaitlistContract.WaitlistEntry.COLUMN_TIMESTAMP  + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP";
+                            ;
 
-    // TODO (4) Create a Constructor that takes a context and calls the parent constructor
+    }
 
-    // TODO (5) Override the onCreate method
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
 
         // TODO (6) Inside, create an String query called SQL_CREATE_WAITLIST_TABLE that will create the table
 
